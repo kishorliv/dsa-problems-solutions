700d1f59edc0921cc86b5dda4a1d2c4ea67ab091
kishor <kishorliv@gmail.com>
2021-08-30 17:58:28 +1000

    Solve merge sorted array problem leetcode.

---
+++ study-plan/data-structure-I/merge-sorted-array.js
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array/
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+    console.log(i, j, k, nums1);
+  }
+  while (i >= 0) nums1[k--] = nums1[i--];
+  while (j >= 0) nums1[k--] = nums2[j--];
+
+  return nums1;
+};
+
+let nums1 = [0],
+  m = 0,
+  nums2 = [1],
+  n = 1;
+const res = merge(nums1, m, nums2, n);
+console.log(res);
